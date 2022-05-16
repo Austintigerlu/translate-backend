1623e50be77e3a39f9619f766a7ac606b4ba68f4
scarletknight13 <lamarbroxton@gmail.com>
2022-05-16 13:18:12 -0500

    initialized cors

--- server.js
+++ server.js
@@ -1,15 +1,17 @@
 require('./config/db.connection.js');
 require('dotenv').config();
-const express = require('express')
-const app = express()
-const db = require('./models')
-const {PORT = 4000} = process.env
+const express = require('express');
+const app = express();
+const db = require('./models');
+const {PORT = 4000} = process.env;
 const cors = require('cors');
 const morgan = require('morgan');
 const controllers = require('./controllers');
 
 
-app.use(express.urlencoded({ extended: false }))
+app.use(express.urlencoded({ extended: false }));
+app.use(cors());
+app.use(morgan());
 app.use('/users', controllers.users);
 app.use('/translations', controllers.translations);
 app.use('/messages', controllers.messages);
