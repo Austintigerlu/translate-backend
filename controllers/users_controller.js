81f3d6bd6614e05d0fdd4dbcc00dbba867eafe49
scarletknight13 <lamarbroxton@gmail.com>
2022-05-17 10:57:09 -0500

    pushing login

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -48,6 +48,7 @@ router.post("/login", (req,res) => {
         }
         bcrypt.compare(userLogin.password, dbUser.password)
         .then(isCorrect => { 
+            console.log(isCorrect)
             if(isCorrect) {
                 const payload = {
                     id: dbUser._id,
@@ -97,8 +98,15 @@ function verifyJWT(req,res,next){
 router.get("/username", verifyJWT, (req,res) => {
     res.json({isLoggedIn: true, username: req.user.Username})
 })
-
- router.delete('/:id', async (req, res) => {
+router.get('/get/:username', async (req, res)=>{
+    try{
+        req.json(await db.User.findOne({username: req.params.username}));
+    }
+    catch(error){
+        res.status(400).json(error);
+    }
+})
+router.delete('/:id', async (req, res) => {
     try {
         const deletedUser = await db.User.findByIdAndDelete(req.params.id);
     } 
